429280e1d76b0d59a8169ca6cfff2f99675a2b0b
23127jk <23127jk@hvhs.school.nz>
2026-04-30 11:00:37 +1200

    score

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ function helloWorld() {
   console.log("Running helloWorld()")
   firebase.database().ref('/').set(
     {
-      message: 'welcome to the chat'
+      message: 'hello world'
     }
   )
 }
 function simpleRead() {
   console.log("reading message()")
   firebase.database().ref('/').child('message').once('value', displayRead,);
-  console.log("leaving simpleRead")
+  console.log("leaving simpleRead");
 
 }
 
@@ -48,19 +48,23 @@ function display(snapshot) {
 }
 
 function fb_readError(error) {
-  console.log ("there was an error reading the message");
+  console.log("there was an error reading the message");
   console.error(error);
 }
 
 function messagecheck() {
   console.log("reading message()")
   firebase.database().ref('/').child('message').once('value', displayRead, fb_readError);
-  console.log("leaving simpleRead");
+  console.log("leaving simpleRead")
 }
 
-function fb_readListener(){
-  console.log("Read listener");
+function fb_readListener() {
+  console.log("Read Listener");
   firebase.database().ref('/message').on('value', fb_logDatabaseRead)
+}
+function fb_logDatabaseRead() {
+  console.log("read logDataBaseRead")
+  firebase.database().ref('/message').on('value', displayRead)
 }
 
 function xiao() {
@@ -72,3 +76,45 @@ function xiao() {
     }
   )
 }
+
+firebase.database().ref('/').set(
+  {
+    game1: {
+      users: {
+        dhruv: 99999,
+        jack: 10000,
+        toby: 9,
+        yug: 987654321,
+      }
+    }
+  }
+);
+
+
+highscoreTable = {
+  game1: {
+    users: {
+      Dhruv: 99999,
+      Jack: 10000,
+      Micheal: "3.141",
+      Toby: 9,
+      Yug: 987654321,
+    }
+  },
+  game2: {
+    users: {
+      Dhruv: 13,
+      Jack: 14,
+      Mikeala: 7,
+      Sasha: 3,
+      Yug: 12,
+    }
+  }
+}
+firebase.database().ref('/').set(highscoreTable)
+firebase.database().ref('/game1/users/jenna/').set(123456789);
+let user = "toby";
+let score = "0";
+firebase.database().ref('/game1/users/' + user).set(
+  score
+);
